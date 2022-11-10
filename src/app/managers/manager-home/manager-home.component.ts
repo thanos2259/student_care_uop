88d036a829366a6da7be9d186e9938197a8baa54
Thanos Konstantopoulos <thanos2259@gmail.com>
2022-11-11 01:39:49 +0200

    basic period add screen added to manager

--- src/app/managers/manager-home/manager-home.component.ts
+++ src/app/managers/manager-home/manager-home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerHomeComponent implements OnInit {
 
+  ngSelect = "";
+  ngSelectPhase = "";
+
   constructor() { }
+
 
   ngOnInit(): void {
   }

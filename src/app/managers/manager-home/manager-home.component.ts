e2ac9c224abc661f4b076ce66ef7a1051be7d266
NasosG <53830262+NasosG@users.noreply.github.com>
2022-11-11 22:29:10 +0200

    right date format for back-end

--- src/app/managers/manager-home/manager-home.component.ts
+++ src/app/managers/manager-home/manager-home.component.ts
@@ -12,16 +12,16 @@ export class CustomAdapter extends NgbDateAdapter<string> {
 		if (value) {
 			const date = value.split(this.DELIMITER);
 			return {
-				day: parseInt(date[0], 10),
+				day: parseInt(date[2], 10),
 				month: parseInt(date[1], 10),
-				year: parseInt(date[2], 10),
+				year: parseInt(date[0], 10),
 			};
 		}
 		return null;
 	}
 
 	toModel(date: NgbDateStruct | null): string | null {
-		return date ? date.day + this.DELIMITER + date.month + this.DELIMITER + date.year : null;
+		return date ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day : null;
 	}
 }
 

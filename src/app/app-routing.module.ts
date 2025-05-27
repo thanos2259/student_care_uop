15bed3af6a4efa8aa50862010fe1b4c797de0e6b
NasosG <53830262+NasosG@users.noreply.github.com>
2025-05-27 21:23:10 +0300

    Added UserWay Accessibility widget + manuals/contact home screen + text above logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,20 @@ import { StudentComponent } from './students/student/student.component';
 import { StudentLoginTermsComponent } from './home-screen/student-login-terms/student-login-terms.component';
 import { AdminPanelComponent } from './admin-panels/admin-panel/admin-panel.component';
 import { AdminPanelLoginComponent } from './admin-panels/admin-panel-login/admin-panel-login.component';
+import { ContactComponent } from './generic-components/contact/contact.component';
+import { ManualsComponent } from './generic-components/manuals/manuals.component';
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+  path: 'manuals',
+  component: ManualsComponent
 },
 {
   path: 'terms',
